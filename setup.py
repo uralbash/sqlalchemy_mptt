6002cceee869db99c7e12f37a9178d1aefd34746
Svintsov Dmitry <root@uralbash.ru>
2019-07-23 14:23:33 +0500

    clean up

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Framework :: Pyramid",
         "Framework :: Flask",
         "Topic :: Internet",

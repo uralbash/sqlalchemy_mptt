ed0eb8f847c02e2ec281ddc6f910cdaaeb79c1bf
uralbash <root@uralbash.ru>
2025-04-30 10:17:47 +0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.mathjax',
-    'sphinx.ext.mathbase',
+    #'sphinx.ext.mathbase',
 ]
 
 # Add any paths that contain templates here, relative to this directory.

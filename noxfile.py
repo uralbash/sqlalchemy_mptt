02d19b6da895cedaf8defc70d0299a98b871716d
Fayaz Yusuf Khan <fayaz.yusuf.khan@gmail.com>
2025-05-19 15:30:04 -0400

    Add test env for Python 3.10

--- noxfile.py
+++ noxfile.py
@@ -1,15 +1,20 @@
 import nox
 
 
-PYTHON_VERSIONS = ["3.8", "3.9"]
+PYTHON_VERSIONS = [(3, 8), (3, 9), (3, 10)]
+SQLALCHEMY_VERSIONS = ["1.0", "1.1", "1.2", "1.3"]
 nox.options.default_venv_backend = "uv"
 
 
-@nox.session(python=[python_version for python_version in PYTHON_VERSIONS] +
-             [f"pypy-{python_version}" for python_version in PYTHON_VERSIONS])
-@nox.parametrize("sqlalchemy", [0, 1, 2, 3])
+@nox.session()
+@nox.parametrize("python,sqlalchemy",
+                 [(f"{interpreter}-{python_major}.{python_minor}", sqlalchemy_version)
+                  for interpreter in ("cpython", "pypy")
+                  for (python_major, python_minor) in PYTHON_VERSIONS
+                  for sqlalchemy_version in SQLALCHEMY_VERSIONS
+                  if sqlalchemy_version >= "1.2" or (python_major, python_minor) <= (3, 9)])
 def test(session, sqlalchemy):
     session.install("pip==24.0")
     session.install("-r", "requirements-test.txt")
-    session.install(f"sqlalchemy~=1.{sqlalchemy}.0")
+    session.install(f"sqlalchemy~={sqlalchemy}.0")
     session.run("pytest", "sqlalchemy_mptt/")

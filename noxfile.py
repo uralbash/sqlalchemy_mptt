5d45d9d2f2d1384d9b024c88e70a6c670f9a1f57
Fayaz Yusuf Khan <fayaz.yusuf.khan@gmail.com>
2025-05-19 19:56:00 -0400

    Add backend for Github Actions

--- noxfile.py
+++ noxfile.py
@@ -3,7 +3,7 @@ import nox
 
 PYTHON_VERSIONS = [(3, 8), (3, 9), (3, 10), (3, 11)]
 SQLALCHEMY_VERSIONS = ["1.0", "1.1", "1.2", "1.3"]
-nox.options.default_venv_backend = "uv"
+nox.options.default_venv_backend = "uv|venv"
 
 
 @nox.session()
